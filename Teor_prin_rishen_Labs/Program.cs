65915a099de9b83a3dc17ceb8e7ac5b46d15da0f
taras-gr <tar.grytsenko@gmail.com>
2020-10-16 08:55:36 +0300

    Add project files.

---
+++ Teor_prin_rishen_Labs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teor_prin_rishen_Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
