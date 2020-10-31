ccd70f6944f41165d05ddc35e9420a31b43bb1f7
taras-gr <tar.grytsenko@gmail.com>
2020-10-31 17:31:08 +0200

    Lab3 is done

---
+++ LabsLibrary/Labs/Lab3/Election.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabsLibrary.Labs.Lab3
+{
+    class Election
+    {
+        public int[,] GetElectionMatrix(string[][] superiorities, int[] votersCounts)
+        {
+            int m = superiorities.GetLength(0);
+            int n = superiorities[0].GetLength(0);
+
+            var candidates = GetListOfCandidatesFromRow<string>(superiorities[3]);
+            var numberOfCandidates = candidates.Count;
+
+            int[,] electionMatrix = new int[numberOfCandidates,
+                numberOfCandidates];
+
+            for (int i = 0; i < numberOfCandidates; i++)
+            {
+                var candidateToProcess = candidates[i];
+
+                for (int j = 0; j < m; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        if (k == i)
+                            continue;
+
+                        if (Array.IndexOf(superiorities[j], candidateToProcess)
+                            < Array.IndexOf(superiorities[j], candidates[k]))
+                        {
+                            electionMatrix[i, k] += votersCounts[j];
+                            electionMatrix[i, i] += votersCounts[j];
+                        }
+
+                    }
+                }
+            }
+
+            return electionMatrix;
+        }
+
+        public int GetWinnerFromElectionMatrixByCondorcet(int[,] electionMatrix)
+        {
+            var m = electionMatrix.GetLength(0);
+            var winsCountForEachCandidate = new int[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (i == j)
+                        continue;
+
+                    if (electionMatrix[i, j] > electionMatrix[j, i])
+                        winsCountForEachCandidate[i]++;
+                }
+            }
+
+            var maxWinsCount = winsCountForEachCandidate.Max();
+            var winnerIndex = Array.IndexOf(winsCountForEachCandidate, maxWinsCount);
+
+            return winnerIndex;
+        }
+
+        public int GetWinnerFromElectionMatrixByBorda(int[,] electionMatrix)
+        {
+            int m = electionMatrix.GetLength(0);
+
+            var winnerIndex = 0;
+
+            for (int i = 1; i < m; i++)
+            {
+                if (electionMatrix[i, i] > electionMatrix[i - 1, i - 1])
+                    winnerIndex = i;
+            }
+
+            return winnerIndex;
+        }
+
+        public List<T> GetListOfCandidatesFromRow<T>(T[] candidateRow)
+        {
+            var list = candidateRow.ToList();
+            list.Sort();
+
+            return list;
+        }
+    }
+}
---
+++ LabsLibrary/Labs/Lab3/Lab3.cs
@@ -0,0 +1,86 @@
+﻿using Common.Core.Interfaces;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace LabsLibrary.Labs.Lab3
+{
+    class Lab3 : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("\n***** Lab3 *****");
+
+            var superiorities = GetMatrixFromFile<string>("superiorities");
+            var votersCounts = GetArrayFromFile("voterCount");
+
+            Election election = new Election();
+
+            var candidates = election.GetListOfCandidatesFromRow(superiorities[0]);
+
+            var electionMatrix = election.GetElectionMatrix(superiorities, votersCounts);
+            int m = electionMatrix.GetLength(0);
+
+            var winnerByCondorcetMethod =
+                election.GetWinnerFromElectionMatrixByCondorcet(electionMatrix);
+            var winnerByBordaMethod =
+                election.GetWinnerFromElectionMatrixByBorda(electionMatrix);
+
+            Console.WriteLine($"\nThere are {candidates.Count} candidates: ");
+            foreach (var candidate in candidates)
+            {
+                Console.Write(candidate + ", ");
+            }
+
+            Console.WriteLine($"\n\nThe superiorities matrix with voters count: ");
+            for (int i = 0; i < superiorities.Length; i++)
+            {
+                Console.Write(votersCounts[i] + " \t");
+                for (int j = 0; j < superiorities[0].Length; j++)
+                {
+                    Console.Write(superiorities[i][j] + $"{((j != superiorities[0].Length - 1) ? " -> "  : string.Empty)}");
+                }
+                Console.WriteLine();
+            }         
+
+            Console.WriteLine("\nThe election matrix is:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(electionMatrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nWinner by Condorcet method is: \t{candidates[winnerByCondorcetMethod]}");
+            Console.WriteLine($"Winner by Borda method is: \t{candidates[winnerByBordaMethod]}");
+        }       
+
+        private T[][] GetMatrixFromFile<T>(string matrixName)
+        {
+            var jsonTestDataFileName = "lab3Data.json";
+            var absolutePathForDocs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory
+                + jsonTestDataFileName);
+
+            var json = File.ReadAllText(absolutePathForDocs);
+            var jObject = JObject.Parse(json);
+            var values = jObject[matrixName]?.ToObject<T[][]>();
+
+            return values != null ? values : throw new ArgumentNullException();
+        }
+
+        private int[] GetArrayFromFile(string arrayName)
+        {
+            var jsonTestDataFileName = "lab3Data.json";
+            var absolutePathForDocs = Path.Combine(AppDomain.CurrentDomain.BaseDirectory
+                + jsonTestDataFileName);
+
+            var json = File.ReadAllText(absolutePathForDocs);
+            var jObject = JObject.Parse(json);
+            var values = jObject[arrayName]?.ToObject<int[]>();
+
+            return values != null ? values : throw new ArgumentNullException();
+        }
+    }
+}
